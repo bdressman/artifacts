829435cd41f23dc1744079374d4b825a3f2b15fb
Brandon Dressman <brandon.dressman@gmail.com>
2026-07-08 15:28:11 -0700

    Slight cleanup of config to include guards and removal of extra file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
 import 'dotenv/config'
 
+const token = process.env.ARTIFACTS_API_TOKEN;
+const characters = process.env.ARTIFACTS_CHARACTERS;
+
+if (!token)
+    throw new Error("ARTIFACTS_API_TOKEN is not set in .env");
+
+if (!characters)
+    throw new Error("ARTIFACTS_CHARACTERS is not set in .env");
+
 export const config = {
-    TOKEN: process.env.ARTIFACTS_API_TOKEN,
-    CHARACTERS: process.env.ARTIFACTS_CHARACTERS.split(","),
+    TOKEN: token,
+    CHARACTERS: characters.split(",").map(c => c.trim()),
 };
