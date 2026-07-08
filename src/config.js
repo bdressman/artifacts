9977f4ad49046f0efb2baca007b14f1f5ea2d8b6
Brandon Dressman <brandon.dressman@gmail.com>
2026-07-08 09:38:30 -0700

    Added character names

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ import 'dotenv/config'
 
 export const config = {
     TOKEN: process.env.ARTIFACTS_API_TOKEN,
+    CHARACTERS: process.env.ARTIFACTS_CHARACTERS.split(","),
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { config } from './config.js';
 
 console.log(config.TOKEN ? "API Token Loaded" : "API Token Missing");
+console.log(config.CHARACTERS ? `Characters: ${config.CHARACTERS}` : "No character listing");
