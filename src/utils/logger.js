1ef55a3b1e13805bbe60a127fe79fdb0483edc84
Brandon Dressman <brandon.dressman@gmail.com>
2026-08-05 01:42:11 -0700

    Simple logging to file with timestamps and messages

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,15 +4,53 @@ const log_dir = "./logs";
 const log_file = `${log_dir}/artifacts.log`;
 await fs.mkdir(log_dir, { recursive: true });
 
-async function write_log(message) {
+
+
+async function write_log(level, message) {
     try {
-        await fs.appendFile(log_file, `${message}\n`);
+
+        // ex: 08/05/2026, 01:32:39 AM
+        const timestamp = new Date().toLocaleString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: true,
+        });
+
+        await fs.appendFile(log_file, `[${timestamp}] [${level}] ${message}\n`);
     } catch (error) {
         console.error("Could not write to log file.", error);
     }
 }
 
-export function log(message) {
-    return write_log(message);
+// Normal progress information
+function info(message) {
+    return write_log("INFO", message);
 }
 
+// Expected game-state conditions
+function warn(message) {
+    return write_log("WARN", message);
+}
+
+// Unexpected technical failures
+function fail(message) {
+    return write_log("FAIL", message);
+}
+
+// Intentional shutdown
+async function halt(message) {
+    await write_log("HALT", message);
+
+    process.exit(1);
+}
+
+export default {
+    info,
+    warn,
+    fail,
+    halt,
+};
--- src/utils/test_logger.js
+++ src/utils/test_logger.js
@@ -1,3 +1,9 @@
-import { log } from "./logger.js";
+import logger from "./logger.js";
 
-log("This is a test of the logging.");
+// Mainly used for quick and easy verification of log file creation and messages.
+// Not extensive testing.
+
+logger.info("Game Information");
+logger.warn("Expected game state");
+logger.fail("Unexpected failure");
+logger.halt("Pack your bags");
